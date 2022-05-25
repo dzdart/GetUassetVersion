6dc2ed020527acc000ad6ee777fa3770939263f3
dzd <dzdlxy@foxmail.com>
2022-05-25 18:39:50 +0800

    删除了无用信息

--- GetUassetVersion.py
+++ GetUassetVersion.py
@@ -8,12 +8,10 @@ if file.endswith(".uasset"):
             new_data+=str(data.read(i),encoding="utf-8")
         except:
             pass
-
-
-    #data=str(data,encoding="utf-8",errors="replace")
-    print(new_data)
     index=new_data.find("UE")
-    print("该文件相关版本信息如下\n",new_data[index-20:index+20])
+    info=new_data[index-20:index+20]
+    info.replace(" ",'')
+    print("该文件相关版本信息如下\n",info)
 else:
     print("务必放入正确的uasset文件！")
 os.system("pause")
