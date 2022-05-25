799a40fb5d52faaa04126a089251342cee806df3
dzd <dzdlxy@foxmail.com>
2022-05-25 18:48:46 +0800

    允许循环检测

--- GetUassetVersion.py
+++ GetUassetVersion.py
@@ -1,17 +1,19 @@
 import os
-file=input("将文件拖入这儿然后回车！")
-if file.endswith(".uasset"):
-    new_data=""
-    data=open(file,"rb")
-    for i in range(3200):
-        try:
-            new_data+=str(data.read(i),encoding="utf-8")
-        except:
-            pass
-    index=new_data.find("UE")
-    info=new_data[index-20:index+20]
-    info.replace(" ",'')
-    print("该文件相关版本信息如下\n",info)
-else:
-    print("务必放入正确的uasset文件！")
-os.system("pause")
+import re
+while 1:
+    file=input("将文件拖入这儿然后回车！")
+    if file.endswith(".uasset"):
+        new_data=""
+        data=open(file,"rb")
+        for i in range(3200):
+            try:
+                new_data+=str(data.read(i),encoding="utf-8")
+            except:
+                pass
+        index=new_data.find("UE")
+        info=new_data[index-20:index+20]
+        info.replace(" ",'')
+        info=re.sub(u"([^\u0041-\u005a\u0061-\u007a\u0030-\u0039])", "", info)
+        print("该文件相关版本信息如下\n",info)
+    else:
+        print("务必放入正确的uasset文件！")
